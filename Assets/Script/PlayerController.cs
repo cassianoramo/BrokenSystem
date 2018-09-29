131642e68e8a5722d339cabecc66c909371b53fe
CassianoRamos <30438171+CassianoRamos@users.noreply.github.com>
2018-09-29 00:09:30 -0300

    Wall Slide v1.0

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,11 +8,13 @@ public class PlayerController : MonoBehaviour {
 	private Animator anim;
 	private Rigidbody2D rb2d;
 	public Transform posPe;
-	public Transform posSide;
+	public LayerMask Ground;
+	public Transform WallCheck;
+	[HideInInspector] public bool touchWall = false;
 	[HideInInspector] public bool tocaChao = false;
 	public float Velocidade;
 	public float ForcaPulo = 1000f;
-	[HideInInspector] public bool viradoDireita = true;
+	[HideInInspector] public bool viradoDireita = true, Jumping;
 	public bool jump;
 	public BoxCollider2D bc;
 	public BoxCollider2D slidecol;
@@ -30,8 +32,8 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		//The groundcheck
 		tocaChao = Physics2D.Linecast (transform.position, posPe.position, 1 << LayerMask.NameToLayer ("Ground"));
+		touchWall = Physics2D.Linecast (transform.position, WallCheck.position, 1 << LayerMask.NameToLayer ("Ground"));
 		Fall ();
-	
 		if (Input.GetKeyDown("space")) {
 			bc.size = new Vector3 (0.5509329f, 0.820936f, 0);
 			bc.offset = new Vector3 (0.1267829f, 0.820936f, 0);
@@ -44,6 +46,13 @@ public class PlayerController : MonoBehaviour {
 	}
 	void FixedUpdate()
 	{
+		if( touchWall ) {
+			anim.SetBool ("Wall Slide", true);
+			anim.SetBool ("Fall", false);
+		}else if(rb2d.velocity.y >= 0){
+			anim.SetBool ("Wall Slide", false);
+			anim.SetTrigger("Jump");
+		}
 		LayerControl ();
 		//Player moviment
 		float translationY = 0;
@@ -61,34 +70,40 @@ public class PlayerController : MonoBehaviour {
 			bc.offset = new Vector3 (-0.07013941f, -0.1166899f, 0);
 		}
 		//Player direction
-		if (translationX > 0 && !viradoDireita) {
+		if (translationX > 0 && !viradoDireita || translationX < 0 && viradoDireita) {
+			if (touchWall && !tocaChao) {
+				rb2d.velocity = (new Vector2 (rb2d.velocity.x, 0f));
+				rb2d.AddForce (new Vector2 (0f, ForcaPulo));
+			}
 			Flip (); 
-		} else if (translationX < 0 && viradoDireita)
-			Flip (); 
-		if (translationX > 0 && !viradoDireita) {
-			Flip ();
-		} else if (translationX < 0 && viradoDireita) {
-			Flip ();
 		}
 }
 	void Fall()
 	{
-		if (!tocaChao && rb2d.velocity.y <= 0) {    
+		if (!tocaChao && rb2d.velocity.y <= 0 && !touchWall) {   
+			if (touchWall) {
+				anim.SetBool ("Wall Slide", true);
+				anim.SetBool ("Fall", false);
+			}else
 			anim.SetBool ("Fall", true);
 			anim.ResetTrigger ("Jump");
+			anim.SetBool ("Wall Slide", false);
 			bc.size = new Vector3 (0.4929347f, 0.9783585f, 0);
 			bc.offset = new Vector3 (0.1226404f, -0.0627588f, 0);
 			Debug.Log ("Fall");
-		}
+		}	
 		if (tocaChao) {
 			Debug.Log ("tocachao");
 			anim.SetBool ("Fall", false);
+			anim.SetBool ("Wall Slide", false);
 		}
 	}
 	void Jump(){
-		if (tocaChao && rb2d.velocity.y >= 0) {
+		if (tocaChao && rb2d.velocity.y >= 0 ) {
 			rb2d.AddForce (new Vector2 (0f, ForcaPulo));
 			anim.SetTrigger ("Jump");
+			anim.SetBool ("Wall Slide", false);
+			Jumping = true;
 		}
 }
 	//Flip script
@@ -120,7 +135,6 @@ public class PlayerController : MonoBehaviour {
 		bc.enabled = true;
 		isSlide = false;
 		}
-
 	 void  OnTriggerEnter2D (Collider2D other){
 		if (other.gameObject.CompareTag ("Obstacle")) {
 			if (tocaChao) {
